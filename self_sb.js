c90b8c9f6d0071a2d6c3aa21262419a19349a9b1
nta45 <nathantayele@gmail.com>
2023-03-12 20:38:16 -0400

    added self_sb files

---
+++ self_sb.js
@@ -0,0 +1,47 @@
+"use strict"
+
+logToConsole();
+
+var COMPONENTS = new Map([ 
+    ['Add Number', {"c":"num","id":"X","v":[{v:0, "id": "ident", input:1},{"c":"btn", "id": "Add"}]}],
+    // ['Add "if" Condition', {"c":"btn", "v": []}]
+]);
+
+var latestcode = '{"v":[\n  \n]}\n';
+var newcode = '{"v":[\n  \n]}\n';
+
+function userEvent(event){
+    if(event.t==0){ // handshake
+        app.display({
+            cap:'UINL screen builder',
+            require:{c:['grid','opt']},
+            style:'self_sb.css',
+            value: [
+                {id:'Your space'},
+                {id:"Items to Add", v:[...COMPONENTS.keys()].map(x=>({id:x, c:'btn'}))},
+            ]
+        })
+    } else{ // after handshake 
+        var itemid = event.u;
+        var itemval = event.v;
+        var dUpdates = [{U:'Warning', v:null}];
+
+        if(COMPONENTS.has(itemid)){
+            let preview=JSON.parse(latestcode); // parse the JSON
+            //check if there is already code, if not create empty
+            if(!preview.v || preview.v.constructor!==Array)preview.v=[];
+            
+            preview.v.push(COMPONENTS.get(itemid));
+            latestcode=JSON.stringify(preview,null,2);
+            // displayUpdates.push({U:'UINLcode',v:lastValidUINLcode});
+            preview.id='Your space';
+            preview.i=1;
+            // uinl.df={on:{pc:[]}};
+            dUpdates.push({U:'Your space',v:null},{add:[preview]});
+        }
+        if(itemid==='Add'){
+            dUpdates.push({U:'Your space', add:[{"c":"txt","v":"number = " + itemval }]});
+        }
+        app.display({Q:dUpdates});
+    }
+}
