39ff29591cf56d2391f8e110873d145391e0a93a
nta45 <nathantayele@gmail.com>
2023-03-29 02:55:33 -0400

    updated Code Object
    needed a way to store user's variable's in order to check for existing variable (data validation) and for ease in future expansion. Created a Code Object and store input there and render it to the JavaScript Code Board.

--- self_sb.js
+++ self_sb.js
@@ -4,23 +4,24 @@ logToConsole();
 
 let COMPONENTS = new Map([
   ["Add a Text Variable", {c: "bin", id: "Add a Variable", 
-                    v: [{ v: " ", id: "inpt1", cap:"Name", input: 1 },{ v: " ", id: "inpt2", cap:"Value", input: 1 },
-                        { c: "btn", id: "add1", cap:"Add" }]}],
+                    v: [{ v: " ", id: "inpt_addtxt_name", cap:"Name", input: 1 },{ v: " ", id: "inpt_addtxt_value", cap:"Value", input: 1 },
+                        { c: "btn", id: "btn_addtextvar", cap:"Add" }]}],
   ["Add a Number Variable", {c: "bin", id: "Add a Variable", 
-                    v: [{ v: " ", id: "inpt4", cap:"Name", input: 1 },{ v: " ", id: "inpt5", cap:"Value", input: 1 },
-                        { c: "btn", id: "add4", cap:"Add" }]}],
+                    v: [{ v: " ", id: "inpt_addnum_name", cap:"Name", input: 1 },{ v: " ", id: "inpt_addnum_value", cap:"Value", input: 1 },
+                        { c: "btn", id: "btn_addnumvar", cap:"Add" }]}],
   ["Add Text to Display", {c: "bin", id: "Add Text to Display", 
-                    v: [{ v: " ", id: "inpt3", cap:"Text to Display ↓", input: 1 }, 
+                    v: [{ v: " ", id: "inpt_print_value", cap:"Text to Display ↓", input: 1 }, 
                     "If you want to print a variable you've already set, type the name of the variable",
-                        { c: "btn", id:"add2", cap: "Add" }]}],
+                        { c: "btn", id:"btn_println", cap: "Print" }]}],
   ["Save JavaScript File", {c: "bin", id: "Download the JavaScript File?", 
                     v:["Are you sure you want to download the JavaScript file?",
-                        { c: "btn", id: "add3", cap:"Yes"}]}]
+                        { c: "btn", id: "btn_savejsfile", cap:"Yes"}]}]
 ]);
 
 let latestcode = '{"v":[\n  \n]}\n';
 let jscode = "";
-let inpt1, inpt2, inpt3, inpt4, inpt5;
+let inpt_addtxt_name, inpt_addtxt_value, inpt_print_value, inpt_addnum_name, inpt_addnum_value;
+let code = {screens:[],functions:{home:{vars:[], commands:[]}}};
 
 function userEvent(event) {
   if (event.t == 0) { // at handshake 
@@ -44,43 +45,91 @@ function userEvent(event) {
       preview.i = 0;
       dUpdates.push({ U: "Your space", v: null }, { add: [preview] });
     } 
-    else if (itemid === "inpt1") { inpt1 = itemval;} 
-    else if (itemid === "inpt2") { inpt2 = itemval;}
-    else if (itemid === "inpt3") { inpt3 = itemval;}
-    else if (itemid === "inpt4") { inpt4 = itemval;}
-    else if (itemid === "inpt5") { if (Number.isFinite(inpt5)) inpt5 = itemval; else{dUpdates.push({U:'Your space',add:[{id:'Warning',v:'Please enter a valid number.'}]});inpt5 = 0}}
-    else if (itemid === "add3") { // if save button clicked
+    else if (itemid === "inpt_addtxt_name") { inpt_addtxt_name = itemval;} 
+    else if (itemid === "inpt_addtxt_value") { inpt_addtxt_value = itemval;}
+    else if (itemid === "inpt_print_value") { inpt_print_value = itemval;}
+    else if (itemid === "inpt_addnum_name") { inpt_addnum_name = itemval;}
+    else if (itemid === "inpt_addnum_value") { inpt_addnum_value = Number(itemval);}
+    else if (itemid === "btn_savejsfile") { // if save button clicked
+      for(const element of code.functions.home.vars) {
+        if (element.type === "text") {
+          jscode += ("let " + element.name + " = \"" + element.value + "\";");
+        } else {
+          jscode += ("let " + element.name + " = " + element.value + ";");
+        }
+      }
+      for(const element of code.functions.home.commands) {
+        if (element.type === "print") {
+          jscode += ("console.log(\"" + element.value + "\");");
+        }
+      }
       dUpdates.push({save:['javascript.js',jscode]});
     }
-    else if (itemid === "add1") { // if add text variable
-      dUpdates.push({U: "JavaScript Code",add: [{ c: "txt", v: "let " + inpt1.trim() + " = \"" + inpt2.trim() + "\";" }]});
-      jscode += ("let " + inpt1.trim() + " = \"" + inpt2.trim() + "\";");
-      let preview = JSON.parse('{"v":[\n  \n]}\n'); // the next 5 lines clear the 'Your space' Board
-      preview.v.push(COMPONENTS.get(itemid));
-      preview.id = "Your space";
-      preview.i = 0;
-      dUpdates.push({U:"Your space", v:null },{add:[preview]});
-    }
-    else if (itemid === "add4") { // if add num variable
-      dUpdates.push({U: "JavaScript Code",add: [{ c: "txt", v: "let " + inpt4.trim() + " = " + inpt5.trim() + ";" }]});
-      jscode += ("let " + inpt4.trim() + " = " + inpt5.trim() + ";");
-      let preview = JSON.parse('{"v":[\n  \n]}\n'); // the next 5 lines clear the 'Your space' Board
-      preview.v.push(COMPONENTS.get(itemid));
-      preview.id = "Your space";
-      preview.i = 0;
-      dUpdates.push({U:"Your space", v:null },{add:[preview]});
-    }
-    else if (itemid === "add2") { // if add print statement
-      dUpdates.push({U:"JavaScript Code", add:[{ c: "txt", v: "console.log(" + inpt3.trim() + ");"}]});
-      jscode += ("console.log(" + inpt3.trim() + ");");
-      let preview = JSON.parse('{"v":[\n  \n]}\n'); // the next 5 lines clear the 'Your space' Board
-      preview.v.push(COMPONENTS.get(itemid));
-      preview.id = "Your space";
-      preview.i = 0;
-      dUpdates.push({U:"Your space", v:null },{add: [preview] });
-    }
     
+    else if (itemid === "btn_addtextvar") { // if add text variable
+      if (code.functions.home.vars.length === 0) {
+        code.functions.home.vars.push({type:"text",name:inpt_addtxt_name.trim(),value:inpt_addtxt_value.trim()});
+      } else {
+        let nameFound = false;
+        for (const element of code.functions.home.vars) {
+          if (element.name === inpt_addtxt_name.trim()) {
+            element.value = inpt_addtxt_value.trim();
+            nameFound = true;
+            dUpdates.push({U:"Your space", v:[]});
+            break;
+          } 
+        }
+        if(!nameFound){
+            code.functions.home.vars.push({type:"text",name:inpt_addtxt_name.trim(),value:inpt_addtxt_value.trim()});
+        }
+        
+      }
+      dUpdates.push({U:"Your space", v:[]});
+    }
+    else if (itemid === "btn_addnumvar") { // if add num variable
+      if (Number.isFinite(inpt_addnum_value)) {
+        if (code.functions.home.vars.length === 0) {
+          code.functions.home.vars.push({type:"number",name:inpt_addnum_name.trim(),value:inpt_addnum_value});
+        } else {
+          let numberFound = false;
+          for (const element of code.functions.home.vars) {
+          if (element.name === inpt_addnum_name.trim()) {
+            element.value = inpt_addnum_value;
+            numberFound = true;
+            dUpdates.push({U:"Your space", v:[]});
+            break;
+          } 
+        }
+        if(!numberFound){
+            code.functions.home.vars.push({type:"number",name:inpt_addnum_name.trim(),value:inpt_addnum_value});
+        }}
+  
+      dUpdates.push({U:"Your space", v:[]});
+      } 
+      else{dUpdates.push({U:'Your space',add:[{id:'Warning',v:'Please enter a valid number.'}]})
+      }
+    }
+    else if (itemid === "btn_println") { // if add print statement
+      code.functions.home.commands.push({type:"print",value:inpt_print_value.trim()});
+      dUpdates.push({U:"Your space", v:[]});
+    }
+      // clearing the javascript code board and updating with the latest values from the Code Object
+      dUpdates.push({U: "JavaScript Code",v:[]});
+      for(const element of code.functions.home.vars) {
+        if (element.type === "text") {
+          dUpdates.push({U: "JavaScript Code",add: [{ c: "txt", v: "let " + element.name + " = \"" + element.value + "\";" }]});
+        } else {
+          dUpdates.push({U: "JavaScript Code",add: [{ c: "txt", v: "let " + element.name + " = " + element.value + ";" }]});
+        }
+      }
+      for(const element of code.functions.home.commands) {
+        if (element.type === "print") {
+          dUpdates.push({U: "JavaScript Code",add: [{ c: "txt", v: "console.log(\"" + element.value + "\");" }]});
+        }
+      }
+    // }
     app.display({ Q: dUpdates });
+    console.log(code);
   }
   
 }
